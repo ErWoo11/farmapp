d834597282a16a74973a1e61d1d074ec9a268a2f
ErWoo11 <latribu_woo11@hotmail.com>
2025-11-25 21:17:44 +0100

    Add files via upload

--- andalucia/sevilla/lascabezas.js
+++ andalucia/sevilla/lascabezas.js
@@ -60,7 +60,7 @@ const calendarData2025 = {
     agosto: [],
     septiembre: [],
     octubre: [],
-    noviembre: ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'red', 'red', 'red'],
+    noviembre: ['orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'red', 'red'],
     diciembre: ['red', 'red', 'red', 'red', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray']
 };
 
